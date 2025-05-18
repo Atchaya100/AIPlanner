16a294a51948ec27d0db1b9723c622a53e9ea739
Atchaya <sakshaya@Ss-MacBook-Air.local>
2025-05-18 21:57:30 +0530

    prompt improved

--- app/routes/openai.py
+++ app/routes/openai.py
@@ -7,33 +7,63 @@ from pydantic import BaseModel
 
 load_dotenv()
 router = APIRouter()
-from groq import Groq
+# from groq import Groq
 
 
 
-client = Groq(
-    api_key=os.environ.get("GROQ_API_KEY"),
-)
+# client = Groq(
+#     api_key=os.environ.get("GROQ_API_KEY"),
+# )
 
 class PromptRequest(BaseModel):
     prompt: str
 
 
+# @router.post("/ask")
+# async def ask_openai(data: PromptRequest):
+#     try:
+#         response = client.chat.completions.create(
+#     messages=[
+#         {
+#             "role": "user",
+#             "content": data.prompt,
+#         }
+#     ],
+#     model="llama-3.3-70b-versatile",
+#     )
+#         return {"response": response.choices[0].message.content}
+#     except Exception as e:
+#         return {"error": str(e)}
+
+
+import requests
+
+url = "https://api.perplexity.ai/chat/completions"
+
 @router.post("/ask")
 async def ask_openai(data: PromptRequest):
     try:
-        response = client.chat.completions.create(
-    messages=[
-        {
-            "role": "user",
-            "content": data.prompt,
-        }
-    ],
-    model="llama-3.3-70b-versatile",
-    )
-        return {"response": response.choices[0].message.content}
+        payload = {
+              "model": "sonar",
+              "messages": [
+                    {
+                        "role": "system",
+                        "content": "Be precise and concise."
+                    },
+                    {
+                        "role": "user",
+                        "content": data.prompt
+                    }
+                    ]
+                }
+        headers = {
+            "Authorization": "Bearer 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",
+            "Content-Type": "application/json"
+            }
+        response = requests.request("POST", url, json=payload, headers=headers)
+        
+        return {"response": response.text}
     except Exception as e:
         return {"error": str(e)}
 
 
-    
